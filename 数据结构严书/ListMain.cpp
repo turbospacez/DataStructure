f3cff0891e383e7218e6ed0d029cd239c7472579
turbospacez <turbospacez@qq.com>
2025-06-23 23:00:45 +0800

    数据结构 严书1 6.23.25

---
+++ 数据结构严书/ListMain.cpp
@@ -0,0 +1,11 @@
+#include "stdds.h"
+
+
+
+int main()
+{
+
+    return 0;
+}
+
+
---
+++ 数据结构严书/stdds.h
@@ -0,0 +1,23 @@
+// 数据结构严蔚敏版
+// 头文件 + 状态码
+// 来源《数据结构算法解析》高一凡
+
+
+#include<string.h> // 字符串函数头文件
+#include<ctype.h> // 字符函数头文件
+#include<stdlib.h> // exit, atoi()
+#include<limits.h> // 最大最小值
+#include<stdio.h> // 输入输出头文件
+#include<io.h> // eof
+#include<math.h> // 数学函数头文件
+#include<sys/timeb.h> // ftime
+#include<stdarg.h> // 存取变长函数
+#include<malloc.h> // malloc
+
+#define TURE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+
+typedef int Status; // 函数类型
+typedef int Boolean; // 布尔类型函数
